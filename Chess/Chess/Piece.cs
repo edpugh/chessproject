b4a42d6c39fa3a25b4aac55ac4348cc6837c7a1a
edpugh <54809657+edpugh@users.noreply.github.com>
2022-09-08 10:50:05 +0100

    chess

---
+++ Chess/Chess/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    class Piece
+    {
+        
+
+        public Piece()
+        {
+
+        }
+    }
+}
